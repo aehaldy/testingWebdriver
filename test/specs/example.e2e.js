5c0778698f1781c127a3f4877e71763eefab967f
Amanda Haldy <ahaldy@edanalytics.org>
2021-01-27 12:00:53 -0500

    Setup testing with webdriverio and allure reporting

---
+++ test/specs/example.e2e.js
@@ -0,0 +1,11 @@
+describe('My Login application', () => {
+    it('should login with valid credentials', () => {
+        browser.url('/#/login');
+
+        $('[type="email"]').setValue('testjssummit@gmail.com');
+        $('[type="password"]').setValue('password');
+        $('button[type="submit"]').click(); 
+
+        expect($('=testjssummit')).toBeExisting();
+    });
+});
